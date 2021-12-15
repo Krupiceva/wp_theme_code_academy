bda326c6ab00fb4fd233736f2fade68ada91c1b6
krupiceva <dora.mlinaric94@gmail.com>
2021-12-15 12:12:12 +0100

    Enable google maps with new post type Campus

--- functions.php
+++ functions.php
@@ -98,4 +98,11 @@ function academy_adjust_queries($query){
 }
 add_action('pre_get_posts', 'academy_adjust_queries');
 
+//Enable google maps with API Key from Google
+function my_acf_init() {
+    acf_update_setting('google_api_key', GOOGLE_MAPS_API);
+}
+ 
+add_action('acf/init', 'my_acf_init');
+
 ?>
