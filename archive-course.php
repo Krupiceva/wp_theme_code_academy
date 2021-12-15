456fc81e6997ad72f232396d0d1452774e65a784
krupiceva <dora.mlinaric94@gmail.com>
2021-12-15 02:12:33 +0100

    Reduce duplicate code with pageBanner function

--- archive-course.php
+++ archive-course.php
@@ -1,18 +1,11 @@
 <?php
 
-get_header(); ?>
-
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/hacker-1.jpg'); ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            All Courses
-        </h1>
-      <div class="page-banner__intro">
-        <p>There is something for everyone. Have a look around.</p>
-      </div>
-    </div>
-</div>
+get_header(); 
+pageBanner(array(
+  'title' => 'All Courses',
+  'subtitle' => 'There is something for everyone. Have a look around'
+));
+?>
 
 <div class="container container--narrow page-section">
 <ul class="link-list min-list">
--- functions.php
+++ functions.php
@@ -1,5 +1,37 @@
 <?php
 
+//Function for dinamic page banner
+function pageBanner($args = NULL){
+
+	if(!$args['title']){
+		$args['title'] = get_the_title();
+	}
+	if(!$args['subtitle']){
+		$args['subtitle'] = get_field('page_banner_subtitle');
+	}
+	if(!$args['photo']){
+		if(get_field('page_banner_background_image') AND !is_archive() AND !is_home()){
+			$args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+		} else {
+			$args['photo'] = get_theme_file_uri('/images/hacker-1.jpg');
+		}
+	}
+
+?>
+	<div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php
+           echo $args['photo'];
+        ?>)">
+        </div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']; ?></p>
+            </div>
+        </div>
+    </div>
+<?php }
+
 //Function for loading site resources (styles, scripts)
 function academy_files(){
 	wp_enqueue_script(
@@ -34,6 +66,7 @@ function academy_features(){
 	add_theme_support('post-thumbnails');
 	add_image_size('teacherLandscape', 400, 260, true);
 	add_image_size('teacherPortrait', 480, 650, true);
+	add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'academy_features');
